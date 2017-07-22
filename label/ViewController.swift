1cbd12993350993f15e42a5107855713e06cf922
GREED <greed@GREEDs-MacBook-Pro.local>
2017-07-21 20:28:00 -0700

    Initial Commit

---
+++ label/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  label
+//
+//  Created by GREED on 7/21/17.
+//  Copyright © 2017 GREED. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
